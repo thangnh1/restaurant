7512c4600130f1dca7891d0499db651f0c360786
Bee Nguyen <thangnh.info@gmail.com>
2022-11-18 13:34:52 +0700

    fix add cart

--- db/connect.php
+++ db/connect.php
@@ -1,11 +1,9 @@
 <?php
-$con = mysqli_connect("localhost","root","","retaurant1");
+$con = mysqli_connect("localhost", "root", "", "restaurant1");
 
 // Check connection
-if (mysqli_connect_errno())
-  {
+if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-	  // Change character set to utf8
-	mysqli_set_charset($con,"utf8");
-?>
+}
+// Change character set to utf8
+mysqli_set_charset($con, "utf8");
